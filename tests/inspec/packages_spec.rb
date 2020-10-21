f1483981c18061c1d7078a463061f3eb16ace4b4
diodonfrost <diodon.frost@diodonfrost.me>
2020-10-21 15:57:22 +0200

    test(kitchen-ci): removing kitchen-ci pipeline
    Kitchen-ci is a tool make by Chef for execute
    integration tests, we no longer need it because
    we use Molecule to do this.
    
    This commit remove kitchen-ci pipeline.

--- tests/inspec/packages_spec.rb
+++
@@ -1,9 +0,0 @@
-
-control 'packages-01' do
-  impact 1.0
-  title 'Vscode package'
-  desc 'Text editor Vscode should be installed'
-  describe package('code') do
-    it { should be_installed }
-  end
-end
